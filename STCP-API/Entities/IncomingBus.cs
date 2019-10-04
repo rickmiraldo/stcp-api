44fb2af12c0cd3d7d04b0f845163e8e4488d9651
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-04 17:20:03 +0100

    Create IncomingBus entity

---
+++ STCP-API/Entities/IncomingBus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace STCP_API.Entities
+{
+    public class IncomingBus
+    {
+        public int LineNumber { get; set; }
+
+        public string LineName { get; set; }
+
+        public DateTime EstimatedTime { get; set; }
+
+        public int WaitingTime { get; set; }
+    }
+}

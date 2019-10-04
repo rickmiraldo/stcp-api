5938f68c229773a0be0720cbf87f03b02840c4cb
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-04 17:19:19 +0100

    Create Stop entity

---
+++ STCP-API/Entities/Stop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace STCP_API.Entities
+{
+    public class Stop
+    {
+        public string Name { get; set; }
+
+        public List<IncomingBus> IncomingBuses { get; set; }
+    }
+}

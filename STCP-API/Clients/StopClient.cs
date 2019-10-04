49175bb42e66e83c6115ebf9a7bbb07f13d318e1
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-04 17:20:32 +0100

    Start building StopClient

---
+++ STCP-API/Clients/StopClient.cs
@@ -0,0 +1,27 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace STCP_API.Clients
+{
+    public static class StopClient
+    {
+        private const string stcpEndpoint = "https://www.stcp.pt/pt/itinerarium/soapclient.php?codigo=";
+        private const string stopFilter = "(//table[contains(@id,'smsBusResults')])[1]";
+        private const string errorFilter = "(//div[contains(@class,'msgBox warning')]//span)";
+
+        public static async Task<string> GetNextBuses(string name)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(stcpEndpoint + name);
+            var pageContents = await response.Content.ReadAsStringAsync();
+
+            HtmlDocument pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+
+            var table = pageDocument.DocumentNode.SelectSingleNode(errorFilter).InnerText;
+
+            return table;
+        }
+    }
+}

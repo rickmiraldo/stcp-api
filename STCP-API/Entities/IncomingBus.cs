d7e4f5d31aac1daf7b61100148b997236e8e9dfe
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-05 00:26:39 +0100

    Override ToString methods

--- STCP-API/Entities/IncomingBus.cs
+++ STCP-API/Entities/IncomingBus.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.Text;
 
 namespace STCP_API.Entities
 {
     public class IncomingBus
     {
-        public int LineNumber { get; set; }
+        public string LineNumber { get; set; }
 
         public string LineName { get; set; }
 
         public DateTime EstimatedTime { get; set; }
 
         public int WaitingTime { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(LineNumber);
+            sb.Append("-");
+            sb.Append(LineName);
+            sb.Append(" - ETA: ");
+            sb.Append(EstimatedTime.ToString("HH:mm"));
+            sb.Append(" (");
+            if (WaitingTime == 0)
+            {
+                sb.Append("a passar");
+            }
+            else
+            {
+                sb.Append(WaitingTime);
+                sb.Append(" min");
+            }
+            sb.Append(")");
+
+            return sb.ToString();
+        }
     }
 }
--- STCP-API/Entities/Stop.cs
+++ STCP-API/Entities/Stop.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace STCP_API.Entities
 {
@@ -7,5 +8,18 @@ namespace STCP_API.Entities
         public string Name { get; set; }
 
         public List<IncomingBus> IncomingBuses { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append("\r\n=====\r\n");
+            foreach (var bus in IncomingBuses)
+            {
+                sb.Append(bus.ToString());
+                sb.Append("\r\n");
+            }
+            return sb.ToString();
+        }
     }
 }

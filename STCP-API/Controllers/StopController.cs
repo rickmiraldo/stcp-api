408ac1d5306e21658708ff373966c5ae591ee89f
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-04 17:20:46 +0100

    Simple test on Stop Controller

---
+++ STCP-API/Controllers/StopController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using STCP_API.Clients;
+using System.Threading.Tasks;
+
+namespace STCP_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StopController : Controller
+    {
+        public async Task<string> Index()
+        {
+            var result = await StopClient.GetNextBuses("LION3");
+
+            return result;
+        }
+    }
+}

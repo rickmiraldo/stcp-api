9a52efe481ec5598523875b6b605a7a7e39b7707
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-10 11:55:23 +0100

    New exception

---
+++ STCP-API/Models/Exceptions/InvalidLineNumberException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace STCP_API.Models.Exceptions
+{
+    public class InvalidLineNumberException : Exception
+    {
+        public InvalidLineNumberException(string message)
+            : base("Invalid line number: " + message)
+        {
+        }
+    }
+}

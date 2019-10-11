1d3e9dfdbe18d36ccd8faf1514d6988bed3f12c3
Henrique Miraldo <rickmiraldo@gmail.com>
2019-10-12 00:09:10 +0100

    Add filter when searching for line

--- STCP-API/Models/Clients/LineClient.cs
+++ STCP-API/Models/Clients/LineClient.cs
@@ -1,5 +1,4 @@
-﻿using HtmlAgilityPack;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using STCP_API.Models.Entities;
@@ -8,22 +7,28 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace STCP_API.Models.Clients
 {
     public static class LineClient
     {
-        private const string stcpEndpoint = "https://www.stcp.pt/pt/viajar/linhas/?linha=";
-        private const string stcpEndpoint2 = "&sentido=";
-        private const string stcpEndpoint3 = "&t=horarios";
+        private const string StcpEndpoint = "https://www.stcp.pt/pt/viajar/linhas/?linha=";
+        private const string StcpEndpoint2 = "&sentido=";
+        private const string StcpEndpoint3 = "&t=horarios";
 
-        private const string resultsFilter = "(//div[contains(@id,'bus-stop-results')])";
+        private const string FilterKeyword = "filter";
+        private const string FullKeyword = "full";
+
+        private const string Line505StopLion1Direction0BusName = ""; // TO-DO Get bus name
+        private const string Line505StopLion1Direction1BusName = "H.S.JOÃO CIR";
+
+        private const string ResultsFilter = "(//div[contains(@id,'bus-stop-results')])";
 
         public static async Task<Line> GetStopsFromLine(string lineNumber, string direction, string getIncomingBuses)
         {
-            bool alsoCheckIncomingBuses = getIncomingBuses.Equals("full") ? true : false;
-            string sentidoConsulta = direction.Equals("1") ? "1" : "0";
+            bool alsoCheckIncomingBuses = (getIncomingBuses.Equals(FullKeyword) || getIncomingBuses.Equals(FilterKeyword)) ? true : false;
+            bool filterUnwantedLines = getIncomingBuses.Equals(FilterKeyword) ? true : false;
+            string directionToCheck = direction.Equals("1") ? "1" : "0";
             
             var co = new ChromeOptions();
             co.AddArgument("headless");
@@ -35,12 +40,12 @@ namespace STCP_API.Models.Clients
                 try
                 {
                     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
-                    driver.Navigate().GoToUrl(stcpEndpoint + lineNumber + stcpEndpoint2 + sentidoConsulta + stcpEndpoint3);
+                    driver.Navigate().GoToUrl(StcpEndpoint + lineNumber + StcpEndpoint2 + directionToCheck + StcpEndpoint3);
                     var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
 
                     wait.Until(wt => !string.IsNullOrWhiteSpace(wt.FindElement(By.CssSelector("#bus-stop-results > table > tbody > tr:nth-child(1) > th.paragem")).Text));
 
-                    var resultString = driver.FindElementByXPath(resultsFilter).GetAttribute("outerHTML");
+                    var resultString = driver.FindElementByXPath(ResultsFilter).GetAttribute("outerHTML");
                     var lineDirection = driver.FindElementByXPath("/html/body/div[3]/div[1]/div[1]/table/tbody/tr/td/div/div[1]/div[1]/div/div[2]").Text;
 
                     driver.Close();
@@ -55,7 +60,7 @@ namespace STCP_API.Models.Clients
                     }
 
                     var busStops = new List<Stop>();
-                    busStops = await ParseLineStops(resultString, alsoCheckIncomingBuses);
+                    busStops = await ParseLineStops(resultString, alsoCheckIncomingBuses, filterUnwantedLines, lineNumber, directionToCheck);
 
                     var line = new Line(lineNumber, lineDirection, busStops);
 
@@ -68,7 +73,7 @@ namespace STCP_API.Models.Clients
             }
         }
 
-        private static async Task<List<Stop>> ParseLineStops(string htmlTable, bool alsoCheckIncomingBuses = false)
+        private static async Task<List<Stop>> ParseLineStops(string htmlTable, bool alsoCheckIncomingBuses, bool filterUnwantedLines, string lineNumberToFilter, string direction)
         {
             var stops = new List<Stop>();
 
@@ -94,12 +99,42 @@ namespace STCP_API.Models.Clients
                     zone = splitZone[1].Substring(0, splitZone[1].IndexOf('<'));
                 }
 
-                // TO-DO Add boolean to check for incoming buses if needed
                 var buses = new List<IncomingBus>();
 
                 if (alsoCheckIncomingBuses)
                 {
+                    // If requested, get next buses from all stops
                     var stopNextBuses = await StopClient.GetNextBuses(stopId);
+
+                    // If requested, filter out only the current searched line from the result
+                    if (filterUnwantedLines)
+                    {
+                        var toBeRemoved = new List<IncomingBus>();
+                        foreach (var incomingBus in stopNextBuses.IncomingBuses)
+                        {
+                            if (incomingBus.LineNumber != lineNumberToFilter)
+                            {
+                                toBeRemoved.Add(incomingBus);
+                            }
+                            // Exception in line 505 stop LION1 where both directions stop at the same stop - Do a manual check
+                            if ((lineNumberToFilter == "505") || (stopId == "LION1"))
+                            {
+                                if ((direction == "0") || (incomingBus.LineName == Line505StopLion1Direction1BusName))
+                                {
+                                    toBeRemoved.Add(incomingBus);
+                                }
+                                else if ((direction == "1") || (incomingBus.LineName == Line505StopLion1Direction0BusName))
+                                {
+                                    toBeRemoved.Add(incomingBus);
+                                }
+                            }
+                        }
+                        foreach (var bus in toBeRemoved)
+                        {
+                            stopNextBuses.IncomingBuses.Remove(bus);
+                        }
+                    }
+
                     buses = stopNextBuses.IncomingBuses;
                 }
 
